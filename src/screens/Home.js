724cfb7b1e95bf3b76ddf416adb7d035ba9f855b
nehldotjs <nelsonosuya11@gmail.com>
2024-04-20 22:13:34 +0100

    NEW COMMIT

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
 import ScreenDetector from "../components/ScreenDetector";
 
-import "../styles/home.css"
+import "../styles/home.css";
 
+import heroImg from "../assets/pexels-artem-podrez-5726806.jpg";
 
 function Home() {
   const { screenWidth } = ScreenDetector();
   return (
     <div className="homeWrapper">
-   
+      <div className="heroSection-Wrapper">
+        <div className="shopNowBtn">
+          <button>
+            <p>Shop Now</p>
+          </button>
+        </div>
+        <img src={heroImg} alt="pexels-artem-podrez-5726806.jpg" />
+      </div>
     </div>
   );
 }
