8e9278b1fcb6b820ffbc9931b7ffe7dbdfde23b0
nehldotjs <nelsonosuya11@gmail.com>
2024-05-07 19:30:11 +0100

    NEW COMMIT

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "../styles/login.css";
 
+import model from "../assets/model.png"
+
 function LoginScreen() {
   return (
     <div className="login-wrapper">
       <div className="backgroundImageWrapper">
-        <h3>background image</h3>
+        <img src={model} alt="" />
       </div>
       <div className="signUpForm">
-        <h3>sign up form </h3>
+        <div className="signUpFormContainer">
+          <div className="signupFormInputWrapper">
+            <h3>signUpFormContainer</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
